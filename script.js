bc9f018ac30aab508a1ce363a3e2b07b1ba6c318
Jeff Kim <jeffersontaekim@gmail.com>
2022-01-07 01:39:29 -0800

    Create user specified dimensions on button

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-//Create 16 div rows with 16 squares
-for (let i = 0; i <= 15; i++) {
-    const div = document.createElement('div');
-    div.classList.add('row');
-    for (let j = 0; j<= 15; j++) {
-        const square = document.createElement('div');
-        square.addEventListener('mouseover', () => {
-            square.classList.add('hover');
-        });
-        square.classList.add('square');
-        div.appendChild(square);
-    }
-    const container = document.querySelector('#container');
-    container.appendChild(div);
 
+grid(16);
+//Grid creation that can be called for initial and user dimensions
+function grid(number) {
+    //Creates every row in the grid
+    for (let i = 1; i <= number; i++) {
+        const div = document.createElement('div');
+        div.classList.add('row');
+
+        //Creates each square in each row
+        for (let j = 1; j<= number; j++) {
+            const square = document.createElement('div');
+            square.addEventListener('mouseover', () => {
+                square.classList.add('hover');
+            });
+            square.classList.add('square');
+            div.appendChild(square);
+        }
+        const container = document.querySelector('#container');
+        container.appendChild(div);
+    }
 }
 
 //Button to reset and create new grid
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
+    //Removes all squares that are changed
     const colored = document.querySelectorAll('.hover');
     colored.forEach((div) => {
         div.classList.remove('hover');
-    })
-})
+    });
+    let dimensions = prompt("Enter new grid dimensions:");
+    if (dimensions > 100) {
+        alert('Please enter a number smaller than 100');
+    } 
+    else if (dimensions < 0) {
+        alert('Please enter a valid number');
+    }
+    else {
+        //Removes just rows to reduce runtime
+        let rows = document.querySelectorAll('.row');
+        rows.forEach((div) => {
+            div.remove();
+        })
+        grid(dimensions);
+    }
+
+});
